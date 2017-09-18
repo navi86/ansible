ef11f6156da3c1cfe1019607ecc4409829e2c3be
navi86 <krylov.ivan86@gmail.com>
2017-09-18 21:35:03 +0300

    basic settings

--- Nutanix/create_vm_ntnx.py
+++ Nutanix/create_vm_ntnx.py
@@ -10,6 +10,7 @@ import sys
 import pprint
 import time
 import ssl
+import argparse
 
 # socket timeout in seconds
 TIMEOUT = 30
@@ -77,11 +78,13 @@ class RestApi():
 
 class ApiLibrary:
     def __init__(self):
+        self.cluster_responses = {"200": "success", "201": "success", "202": "success", "400": "input_error",
+                                  "401": "input_error", "403": "input_error", "408": "request_timeout",
+                                  "409": "input_error", "404": "api_not_available", "501": "Internal Server Error"}
         pass
 
-    # Parse a list
-    # list to parse
-    # key for which parse is to be done
+    # toparse : List to be parsed
+    # lookfor : key for which parse is to be done
     def parse_list(self, toparse, lookfor):
         for data in toparse:
             if isinstance(data, dict):
@@ -100,13 +103,14 @@ class ApiLibrary:
                         uuid = self.parse_list(result[key], lookfor)
                         return uuid
                     else:
-                        if type(result[key]) == dict:
+                        if isinstance(result[key], dict):
                             return result[key].get(lookfor, None)
                         return result[key]
                 elif isinstance(result[key], dict):
                     uuid = self.parse_result(result[key], meta_key, lookfor)
                     if uuid:
                         return uuid
+
         return uuid
 
     # Check the return status of API executed
@@ -122,7 +126,8 @@ class ApiLibrary:
             if status:
                 print '*' * 80
                 state = self.parse_result(result, "status", "state")
-                if state == "kError":
+                print state
+                if state == "Error" or "ERROR":
                     print "Reason: ", status.get('reason')
                     print "Message: ", status.get("message")
                 else:
@@ -133,36 +138,41 @@ class ApiLibrary:
     def __is_result_complete(self, status, result):
         if result and str(result.get('code')) == "404":
             return True
-        if result and str(status) == "200":
+        if result and str(status) == "200" or "202":
             api_status = self.parse_result(result, "status", "state")
-            if api_status == "kComplete":
+            print api_status
+            if api_status == "COMPLETE":
                 return True
-            elif api_status == "kError":
+            elif api_status == "Error" or "ERROR":
                 return None
         return False
 
     def track_completion_status(
             self, RestApi, status, result, get_api_status):
-        retry_count = 5
-        wait_time = 2  # seconds
+        start_time = time.clock()
+        retry_count = 100
+        wait_time = 3  # seconds
         uuid = None
 
-        if result and str(status) == "200":
+        if result and str(status) == "200" or "202":
             uuid = self.parse_result(result, "metadata", "uuid")
 
         if self.__is_result_complete(status, result):
             return uuid
         else:
             api_status = self.parse_result(result, "status", "state")
-            if uuid and api_status != "kComplete" and api_status != "kError":
+            if uuid and api_status != "COMPLETE" and api_status != "Error":
                 count = 0
                 while count < retry_count:
                     count = count + 1
                     time.sleep(wait_time)
                     (status, result) = get_api_status(RestApi, uuid)
                     get_status = self.__is_result_complete(status, result)
-                    # API status is kComplete
+                    # API status is COMPLETE
                     if get_status is True:
+                        end_time = time.clock()
+                        total_time_taken = end_time - start_time
+                        print "Total Time taken to complete:", total_time_taken
                         return uuid
                     # API status is Error
                     if get_status is None:
@@ -172,41 +182,6 @@ class ApiLibrary:
             api_status = self.parse_result(result, "status", "state")
             print "API status :", api_status
             return None
-
-
-    def track_deletion_status(self, RestApi, uuid, get_api_status):
-        count = 0
-        api_status = ""
-        status = 0
-        result = None
-        while count < 3:
-            count = count + 1
-            time.sleep(5)
-            (status, result) = get_api_status(RestApi, uuid)
-            if result:
-                if str(status) == "200":
-                    api_status = self.parse_result(result, "status", "state")
-                else:
-                    api_status = result.get('status', None)
-            if api_status == "failure":
-                return True
-        if not str(status) == "200":
-            self.print_failure_status(result)
-            return False
-        else:
-            if api_status == "kComplete":
-                return True
-            elif api_status == "failure":
-                self.print_failure_status(result)
-                return False
-            elif api_status == "kError":
-                print "Reason:", self.parse_result(result, "status", "reason")
-                print "Message:", self.parse_result(result, "status", "message")
-                return False
-            else:
-                print "Timed Out"
-                print result
-                return False
 
 
 def create_vm(restApi, vm_name, network_uuid, cluster_uuid, memory, num_sockets,vcpu,power_state):
@@ -243,13 +218,8 @@ def create_vm(restApi, vm_name, network_uuid, cluster_uuid, memory, num_sockets,
     return status, result
 
 
-def clone_vm_from_image(restApi, vm_name, image_name,subnet_name, cluster_name, memory, num_sockets,vcpu,power_state):
-    # Get image UUID
-    image_uuid = get_image(RestApi, image_name)
-    # Get subnet UUID
-    subnet_uuid = get_network(RestApi, subnet_name)
-    # Get cluster UUID
-    cluster_uuid = get_cluster(RestApi, cluster_name)
+def clone_vm_from_image(restApi, vm_name, image_uuid, subnet_uuid, cluster_uuid, memory, num_sockets, vm_vcpu,
+                        power_state):
     # Get cloud-init config
     configfile = (open("template-cfg.yml")).readlines()
     configreplace = [line.replace('vmname', vm_name) for line in configfile]
@@ -272,7 +242,7 @@ def clone_vm_from_image(restApi, vm_name, image_name,subnet_name, cluster_name, 
             }
           ],
           "power_state": power_state,
-          "num_vcpus_per_socket": int(vcpu),
+          "num_vcpus_per_socket": int(vm_vcpu),
           "num_sockets": int(num_sockets),
           "memory_size_mib": int(memory),
           "guest_customization": {
@@ -284,8 +254,7 @@ def clone_vm_from_image(restApi, vm_name, image_name,subnet_name, cluster_name, 
             {
               "data_source_reference": {
                 "kind": "image",
-                "uuid": image_uuid,
-                "name": image_name
+                "uuid": image_uuid
               },
               "device_properties": {
                 "disk_address": {
@@ -321,8 +290,11 @@ def get_image(restApi, image_name):
     }
     restApi.rest_params_init(sub_url="images/list", method="POST", body=body)
     (status, result) = restApi.rest_call()
-    image_uuid = [image['metadata']['uuid'] for image in result['entities'] if image['status']['name'] == image_name]
-    return status, image_uuid
+    #image_uuid = "".join([image['metadata']['uuid'] for image in result['entities']
+    #                      if image['status']['name'] == image_name])
+    #result = {'status': status, 'uuid': image_uuid.encode('ascii')}
+    #return result
+    return status, result
 
 
 def get_network(restApi, vm_subnet):
@@ -330,8 +302,11 @@ def get_network(restApi, vm_subnet):
     body = {"kind": "subnet"}
     restApi.rest_params_init(sub_url="subnets/list", method="POST", body=body)
     (status, result) = restApi.rest_call()
-    subnet_uuid =[subnet['metadata']['uuid'] for subnet in result['entities'] if subnet['status']['name'] == vm_subnet]
-    return status, subnet_uuid
+    #subnet_uuid = "".join([subnet['metadata']['uuid'] for subnet in result['entities']
+    #                      if subnet['status']['name'] == vm_subnet])
+    #result = {'status': status, 'uuid': subnet_uuid.encode('ascii')}
+    #return result
+    return status, result
 
 
 def get_cluster(restApi, cluster_name):
@@ -339,8 +314,11 @@ def get_cluster(restApi, cluster_name):
     body = {"kind": "cluster"}
     restApi.rest_params_init(sub_url="clusters/list", method="POST", body=body)
     (status, result) = restApi.rest_call()
-    cluster_uuid =[cluster['metadata']['uuid'] for cluster in result['entities'] if cluster['status']['name'] == cluster_name]
-    return status, cluster_uuid
+    #cluster_uuid = "".join([cluster['metadata']['uuid'] for cluster in result['entities']
+    #                       if cluster['status']['name'] == cluster_name])
+    #result = {'status': status, 'uuid': cluster_uuid.encode('ascii')}
+    #return result
+    return status, result
 
 
 # Get a VM with particular UUID.
@@ -350,26 +328,96 @@ def get_vm_by_uuid(restApi, vm_uuid):
     (status, result) = restApi.rest_call()
     return status, result
 
+def api_response_(status, result_list, origin_name):
+    api_library = ApiLibrary()
+    api_response = api_library.cluster_responses
 
+    item_kind = api_library.parse_result(result_list, "metadata", "kind")
+    if api_response[str(status)] == "success":
+        items = result_list.get('entities')
+        #print('%s UUID List' % item_kind)
+        for item in items:
+            item_name = api_library.parse_result(item, "status", "name")
+            if item_name == origin_name:
+                item_uuid = api_library.parse_result(item, "metadata", "uuid")
+                #print('%s uuid: %s' % (item_kind, item_uuid))
+                return item_uuid
+    else:
+        print "Failed to get clusters list."
+        api_library.print_failure_status(result_list)
+        return
+
+
+def main(task, vm_name, image_name, vm_cluster, vm_network, vm_memory, vm_num_sockets, vm_vcpu, vm_power_state, user, password,
+         cvm_address):
+
+    #rest_api = RestApi("ntnx-548b1f14-a-cvm.local", "admin", "1qaz@WSX3edc")
+    rest_api = RestApi(cvm_address, user, password)
+
+    # Get cluster UUID
+    (status, result) = get_cluster(rest_api, vm_cluster)
+    cluster_uuid = api_response_(status, result, vm_cluster)
+
+    # Get image UUID
+    (status, result) = get_image(rest_api, image_name)
+    image_uuid = api_response_(status, result, image_name)
+
+    # Get subnet UUID
+    (status, result) = get_network(rest_api, vm_network)
+    subnet_uuid = api_response_(status, result, vm_network)
+
+    (status, result) = clone_vm_from_image(restApi=rest_api, vm_name=vm_name, image_uuid=image_uuid, subnet_uuid=subnet_uuid,
+                        cluster_uuid=cluster_uuid, memory=vm_memory, num_sockets=vm_num_sockets, vm_vcpu=vm_vcpu,
+                        power_state=vm_power_state)
+    api_library = ApiLibrary()
+    api_response = api_library.cluster_responses
+    if api_response[str(status)] == "success":
+        item_uuid = api_library.parse_result(result, "metadata", "uuid")
+        print(item_uuid)
+        return item_uuid
+    else:
+        print "Failed to get clusters list."
+        api_library.print_failure_status(result)
+        return
 
 if __name__ == '__main__':
+    # Fetch required parameters
+    """parser = argparse.ArgumentParser(description='Create a VM on Nutanix.')
+    parser.add_argument('-name', type=str, dest='vm_name', required=True, help='Virtual machine name')
+    parser.add_argument('-image', type=str, dest='image', required=True, help='Image name')
+    parser.add_argument('-network', type=str, dest='network', required=True, help='Network name')
+    parser.add_argument('-cluster', type=str, dest='cluster', required=True, help='Nutanix cluster name')
+    parser.add_argument('-memory', type=int, dest='memory', required=True, help='Memory size in MB')
+    parser.add_argument('-vcpu', type=int, dest='vcpu', required=True, help='Amount of vCPU')
+    parser.add_argument('-numvcpu', type=int, dest='numvcpu', required=True, help='Amount of sockets')
+    parser.add_argument('-state', type=str, dest='state', required=True, help='VM power state')
+    parser.add_argument('-user', type=str, dest='userNTNX', required=True,
+                        help='User with permission create VM on Nutanix cluster')
+    parser.add_argument('-password', type=str, dest='passNTNX', required=True, help='Password of user')
+    parser.add_argument('-cvm_address', type=str, dest='cvm_address', required=True, help='IP or FQDN of Nutanix cluster')
+    args = parser.parse_args()"""
+    #./create_vm_ntnx.py -name 'ansible-client10' -image 'Centos7-cloudInit' -cluster 'NTNX-lab' -network 'vlan0' -memory 1024 -numvcpu 1 -vcpu 1 -state 'ON' -user 'admin' -password '1qaz@WSX3edc' -cvm_address 'ntnx-548b1f14-a-cvm.local'
 
-    RestApiconnection = RestApi("ntnx-548b1f14-a-cvm.local","admin",
-                                "1qaz@WSX3edc")
+    # Temporal variable
+    vm_name = "ansible-client10"
+    image_name = "Centos7-cloudInit"
+    vm_network = "vlan0"
+    vm_cluster = "NTNX-lab"
+    vm_memory = 1024
+    vm_num_sockets = 1
+    vm_vcpu = 1
+    vm_power_state = "ON"
+    user = 'admin'
+    password = "1qaz@WSX3edc"
+    cvm_address = 'ntnx-548b1f14-a-cvm.local'
+    main('clone', vm_name=vm_name, image_name=image_name, vm_cluster=vm_cluster, vm_network=vm_network,
+         vm_memory=vm_memory, vm_num_sockets=vm_num_sockets, vm_vcpu=vm_vcpu, vm_power_state=vm_power_state,
+         cvm_address=cvm_address, user=user, password=password)
+    ###
 
 
-    vm_name="ansible-client10"
-    image_name="Centos7-cloudInit"
-    vm_network="vlan0"
-    vm_cluster="NTNX-lab"
-    vm_memory=1024
-    vm_num_sockets=1
-    vm_vcpu=1
-    vm_power_state="ON"
-
-#print(clone_vm_from_image(RestApi=RestApiconnection, vm_name="ansible-client10",image_name="Centos7-cloudInit",
-#                    network_uuid="88ba1a7c-5989-43a8-831f-e6355bb2b6d9",
-#                    cluster_uuid="0005558c-536f-7adc-2348-001fc69c242b", memory=1024,
-#                    num_sockets=1, vcpu=1, power_state="ON"))
+    #main('clone', vm_name=args.vm_name, image_name=args.image, vm_cluster=args.cluster, vm_network=args.network,
+    #     vm_memory=args.memory, vm_num_sockets=args.numvcpu, vm_vcpu=args.vcpu, vm_power_state=args.state,
+    #     cvm_address=args.cvm_address, user=args.userNTNX, password=args.passNTNX)
 
 
